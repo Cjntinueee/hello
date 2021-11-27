3647c81c29aaede2dbdfeca4d448b125eddf3901
Cjntinueee <95123379+Cjntinueee@users.noreply.github.com>
2021-11-27 15:29:12 +0000

    0.1

---
+++ YaKrun/Assets/BOXtriggerr.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BOXtriggerr : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }   
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
